e9753663d03236b30fc1170c3dc980e2f08e3a72
Ilvina Sya'rani <64575281+ilvinasyarani@users.noreply.github.com>
2020-05-17 22:39:39 +0800

    Create Jobsheet 9 C++.cpp
    program array sebagai karakter dengan menampilkan nama anggota klp praktikan bersifat Statis

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(int argc, char*argv[])
+{
+string nama[5]; 
+int i,j; 
+nama[0]="Ilvina sya'rani";
+nama[1]="haidir";
+nama[2]="azhar";
+nama[3]="hanif qodri abdullah";
+nama[4]="frederick benaya situmorang";
+
+cout<<"nama 1 = "<<nama[0]<<endl;
+cout<<"nama 2 = "<<nama[1]<<endl;
+cout<<"nama 3 = "<<nama[2]<<endl;
+cout<<"nama 4 = "<<nama[3]<<endl;
+cout<<"nama 5 = "<<nama[4]<<endl;
+
+return 0;
+}
