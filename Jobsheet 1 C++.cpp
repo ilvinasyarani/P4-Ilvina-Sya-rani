4cde36740aac47b3ac21edd82705cc2dad5d66ad
Ilvina Sya'rani <64575281+ilvinasyarani@users.noreply.github.com>
2020-05-17 22:00:08 +0800

    Create Jobsheet 1 C++.cpp
    Program menampilkan akhiran NIM secara Statis

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Ilvina Sya'rani'\n"<<endl;
+	int nim[5]= {1,9,0,6,9};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+}
