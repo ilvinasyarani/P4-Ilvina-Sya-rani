3e08561dbbe779a56f07237f975975a4fae26923
Ilvina Sya'rani <64575281+ilvinasyarani@users.noreply.github.com>
2020-05-17 22:46:31 +0800

    Create Jobsheet 9 C.c
    Program array sebagai karakter dengan menampilkan nama anggota klp praktikan bersifat Statis

---
+++ Jobsheet 9 C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int i;
+	char nama[6]={'V','I','N','A'};
+	char nama1[7]={'F','R','E','D','E','R','I','C','K'};
+	char nama2[10]={'H','A','N','I','F'};
+	char nama3[6]={'A','Z','H','A','R'};
+	char nama4[10]={'H','A','I','D','I','R'};
+
+	
+	printf("\n");
+	printf("Nama = %s\n\n",nama);
+	printf("Nama = %s\n\n",nama1);
+	printf("Nama = %s\n\n",nama2);
+	printf("Nama = %s\n\n",nama3);
+	printf("Nama = %s\n\n",nama4);
+	return 0;
+}
